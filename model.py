536e897b35a3f0e4c90cf25d30d68f72646731a8
resbyte <abhinavvicku@gmail.com>
2018-09-18 21:59:14 +0900

    tests upsampling and downsampling

--- model.py
+++ model.py
@@ -59,7 +59,7 @@ class UpSample(nn.Module):
     def __init__(self, in_channels, out_channels, bilinear=False):
         super(UpSample, self).__init__()
         self.main = nn.Sequential(
-                        nn.Conv2d(in_channels, out_channels, 
+                        nn.Conv2d(in_channels*2, out_channels, 
                             kernel_size=3,stride=1,padding=1),
                         nn.ReLU(),
                         nn.Conv2d(out_channels, out_channels, 
@@ -71,17 +71,25 @@ class UpSample(nn.Module):
         )
         
     def forward(self, x,y):
-        x = torch.cat((x, y), dim=0)
-        print(x.shape)
+        x = torch.cat((x, y), dim=1)
         return self.main(x)
 
+
+class Unet(nn.Module):
+    def __init__(self):
+        super(Unet,self).__init__()
+        
+
 if __name__=='__main__':
-    up_sample = UpSample(3, 32, bilinear=True)
+    
     x = Variable(torch.randn((1, 3, 320, 320)))
-
-    out = up_sample(x, x)
-    print(out.shape)
-
+    print("downsampling")
     down_sample = DownSample(in_channels=3,out_channels=64)
     out_pooled, out = down_sample(x)
     print(out.shape, out_pooled.shape)
+    print("upsampling")
+    up_sample = UpSample(64, 3, bilinear=True)
+    out = up_sample(out_pooled, out_pooled)
+    print(out.shape, x.shape)
+
+    
