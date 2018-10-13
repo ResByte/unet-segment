58e0951bcb3bb480e3ef1548dffda24e9523967c
resbyte <abhinavvicku@gmail.com>
2018-10-13 12:47:41 +0900

    writing unet structure

--- model.py
+++ model.py
@@ -30,7 +30,7 @@ class DownSample(nn.Module):
     """
     def __init__(self, in_channels=3, out_channels=64):
         super(DownSample, self).__init__()
-        
+        # backbone of downsampling 
         self.main = nn.Sequential(
                         nn.Conv2d(in_channels, out_channels, 
                                     kernel_size=3, stride=1, padding=1),
@@ -76,10 +76,74 @@ class UpSample(nn.Module):
 
 
 class Unet(nn.Module):
-    def __init__(self):
-        super(Unet,self).__init__()
+    """
+    Overall unet structure 
+    Ex: for a depth of 1
+    conv3x3 ->conv3x3-->crop_and_add-->conv3x3-->conv3x3-->conv1x1--> out
+                 |                      / \
+                \ /                      |
+            max_pool2x2              upsample2x2
+                 |                      / \
+                \ /                      |
+               conv3x3        -->     conv3x3
+    """
+    def __init__(self,
+        in_channels=3, 
+        nb_filters = 64,
+        depth = 3,
+        nb_classes=10,
+        upsample_mode='bilinear'):
         
+        super(Unet,self).__init__()
+        self.in_channels = in_channels
+        self.nb_filters = nb_filters
+        self.depth = depth
+        self.nb_classes = nb_classes
+        self.upsample_mode = upsample_mode
+        self.down_convs = []
+        self.up_convs = []
 
+        for i in range(depth):
+            if i ==0:
+                ins = self.in_channels
+            else:
+                ins = outs
+            # increase the number of filters as  2^depth
+            outs = self.nb_filters*(2**(i+1)) 
+
+            # create downsampling block for this ins-->outs
+            down_conv = DownSample(ins, outs)
+
+            self.down_convs.append(down_conv)
+        
+        # start with previous depth and create upconv blocks
+        for i in range(depth):
+            ins = outs 
+            outs = ins // 2
+
+            # using bilinear up convolutions
+            up_conv = UpSample(ins, outs, bilinear=True)
+            self.up_convs.append(up_conv)
+        
+        #final layer
+        self.out_conv = nn.Conv2d(outs, self.nb_classes, 
+                            kernel_size=1, stride=1)
+        
+        self.down_convs = nn.ModuleList(self.down_convs)
+        self.up_convs = nn.ModuleList(self.up_convs)
+
+    def forward(self, x):
+       # TODO: add forward pass
+
+       # process downsampling 
+
+       # process cropping of each intermediate downsampling layer 
+
+       # append crops and process upsampling 
+       # return final layers 
+       
+       return  
+        
 if __name__=='__main__':
     
     x = Variable(torch.randn((1, 3, 320, 320)))
