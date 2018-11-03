466d371973628b24ec91c0340f15723ceb9839d2
ResByte <abhinavvicku@gmail.com>
2018-11-04 00:02:30 +0900

    starts dataset module

---
+++ dataset.py
@@ -0,0 +1,16 @@
+import torch
+from torch.utils.data import DataLoader, Dataset
+import torchvision
+
+# TODO : Add ADE20K data loader using pytorch Dataset class
+class ADE20K(Dataset):
+    """ADE20K dataset is from MIT scene parsing challenge. 
+    This is becoming a benchmark for several CV papers. 
+    Reference: http://groups.csail.mit.edu/vision/datasets/ADE20K/
+    """
+    def __init__(self):
+        return NotImplementedError
+
+
+
+    
